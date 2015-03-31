ceefb9d6f0261e2a177a13b5c05d83ba09fdc3a8
Muthukumar Srinivasan <muthukumar.srinivasan@gmail.com>
2015-03-31 13:32:36 -0700

    Week8Project3
    By Muthukumar Srinivasan & Rajagopal Srinivasan

---
+++ AwardCategory.sql
@@ -0,0 +1,56 @@
+﻿--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.4.0
+-- Dumped by pg_dump version 9.4.0
+-- Started on 2015-03-31 11:19:47
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
+-- TOC entry 1994 (class 0 OID 41004)
+-- Dependencies: 172
+-- Data for Name: awardCategory; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "awardCategory" ("Year", "CatergoryName", "CategoryId") FROM stdin;
+2010      	AActor - Supporting Role                                                                            	111
+2010      	Actress Supporting Role                                                                             	112
+2010      	Animated Featured Film                                                                              	113
+2010      	Art Direction                                                                                       	114
+2010      	Cinematography                                                                                      	115
+2010      	Costume Design                                                                                      	116
+2010      	Directing                                                                                           	117
+2010      	Documentary (Feature)                                                                               	118
+2010      	Documentary (Short Subject)                                                                         	119
+2010      	Film Editing                                                                                        	110
+2010      	Foreign Language Film                                                                               	111
+2010      	Makeup                                                                                              	112
+2010      	Music(Scoring)                                                                                      	113
+2010      	Music(Song)                                                                                         	114
+2010      	Best Picture                                                                                        	115
+2010      	ShortFilm(Animated)                                                                                 	116
+2010      	ShortFile(live Action)                                                                              	117
+2010      	Sound                                                                                               	118
+2010      	Sound Editing                                                                                       	119
+2010      	Visual Effects                                                                                      	120
+2010      	Writing                                                                                             	121
+2010      	Actor Leading Role                                                                                  	122
+2010      	Actress-Leading Role                                                                                	123
+\.
+
+
+-- Completed on 2015-03-31 11:19:48
+
+--
+-- PostgreSQL database dump complete
+--
+
